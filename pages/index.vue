2f1897509b09674f72b1c4ff3f6b008bad3843a1
Suzuki Takayuki <takayuki_suzuki@rebuild-inc.co.jp>
2020-09-17 15:59:55 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+<script>
+  import axios from 'axios'
+  export default {
+    async asyncData() {
+      const { data } = await axios.get(
+// your-service-id部分は⾃分のサービスidに置き換えてください
+        'https://bumptakayuki.microcms.io/api/v1/blog',
+        {
+// your-api-key部分は⾃分のapi-keyに置き換えてください
+          headers: { 'X-API-KEY': '5383df4d-d380-47c8-aaa7-cd7e6bda8a5d' }
+        }
+      )
+      return data
+    }
+  }
+</script>
